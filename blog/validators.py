932ed891d75d4ffd0583effdbae2144ce8990312
ParkYeonJun <pyj8127@gmail.com>
2016-08-01 13:15:44 +0900

    우체국 API 이용 ZipCodeValidator 클래스 제작

--- blog/validators.py
+++ blog/validators.py
@@ -1,12 +1,14 @@
 import re, sqlite3
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'programming.settings')
+import requests
 from django.forms import ValidationError
+from django.conf import settings
 from django.core.validators import MinLengthValidator
 from django.utils.deconstruct import deconstructible
 import sys
 from poketmongo.models import Zipcode
-
+import xmltodict
 
 @deconstructible
 class MinLengthValidator(object):
@@ -34,3 +36,36 @@ def zip_code_validator(zip_code):
 def lnglat_validator(lnglat):
     if not re.match(r'^(\d+\.?\d*),(\d+\.?\d*)$', lnglat):
         raise ValidationError('Invalid LngLat Type')
+
+@deconstructible
+class ZipCodeValidator(object):
+    '우편번호 체계안내 : http://www.koreapost.go.kr/kpost/sub/subpage.jsp?contId=010101040100'
+
+    def __init__(self, is_check_exist =False):
+        self.is_check_exist = is_check_exist
+
+    def __call__(self, zip_code):
+        if not re.match(r'^\d{5}$', zip_code):
+            raise ValidationError('5자리 숫자로 입력해주세요.')
+
+        if self.is_check_exist:
+            self.check_exist(zip_code)
+
+    def check_exist(self, zip_code):
+        '우체국 open api : http://biz.epost.go.kr/customCenter/custom/custom_10.jsp'
+
+        params = {
+            'regkey': settings.EPOST_API_KEY,
+            'target': 'postNew',
+            'query': zip_code,
+        }
+        xml = requests.get('http://biz.epost.go.kr/KpostPortal/openapi', params=params).text
+        response = xmltodict.parse(xml)
+        try:
+            error = response['error']
+        except KeyError:
+            pass
+        else:
+            raise ValidationError('[{error_code}] {message}'.format(**error))
+
+
