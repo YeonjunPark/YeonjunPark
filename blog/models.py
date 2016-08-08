053081380f842c34eddc19602a3fb0beefff921c
ParkYeonJun <pyj8127@gmail.com>
2016-08-08 16:28:44 +0900

    2016.8. 8. 실습 코드 주석

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,10 @@ from blog.utils import square_image, thumbnail
 def random_name_upload_to(instance, filename):
     name = uuid4().hex
     extension = os.path.splitext(filename)[-1].lower()
+     # splitext = split extension, 경로와 확장자를 나누어 튜플로 반환)
+     # 경로를 반환할 때 파일의 확장자는 변하면 안되기 때문에 이 메소드를 사용.
     return os.path.join(name[:3], name[3:6], name[6:] + extension)
+    # name[:3] + '/' + name[3:6] + '/' + name[6:] + 확장자. os.path.join에서는 ,는 /로 연결되고 +는 그대로 뒤에 이어 붙인다.
 
 '''
 def min_length_validator(min_lentgh):
@@ -86,14 +89,21 @@ class Zipcode(models.Model):
 
 def pre_on_post_save(sender, **kwargs):
     post = kwargs['instance']
-    if post.photo:
+    if post.photo:     # 사진이 저장된 경로가 존재한다면
+         # post.photo : 이미지 저장 경로
+         # post.photo.name : 이미지 파일명
+         # post.photo.path : 이미지 저장 absolute url
+         # post.photo.url : 이미지 url
+         # post.photo.file : 경로에 있는 파일에 대하여 읽고, 쓸 수 있는 기능을 제공
+         # post.photo.with (ImageField only)
+         # post.photo.height (ImageField only)
         max_width = 300
         if post.photo.width > max_width or post.photo.height > max_width:
             processed_file = thumbnail(post.photo.file, max_width, max_width)
             # processed_file = square_image(post.image.file, max_width)
             post.photo.save(post.photo.name, File(processed_file))
 
-pre_save.connect(pre_on_post_save, sender=Post)
+pre_save.connect(pre_on_post_save, sender=Post) # Post모델이 호출되고 저장하기 직전에 on_pre_save라는 함수를 호출하겠다.
 
 
 
